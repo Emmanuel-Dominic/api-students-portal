cef084e99bb1ab45eb9de160321f6909c7101df3
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-08-21 16:44:26 +0300

    chore: register models to admin access

--- portal/apps/courses/admin.py
+++ portal/apps/courses/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Course
+
 
 # Register your models here.
+class CourseAdminModel(admin.ModelAdmin):
+    # models = Student
+    list_display = (
+        "name",
+        "description",
+    )
+    search_fields = (
+        "name",
+    )
+    readonly_fields = (
+        "id",
+    )
+
+
+admin.site.register(Course, CourseAdminModel)
--- portal/apps/users/admin.py
+++ portal/apps/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.contrib.admin.models import LogEntry
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+from .models import Student
+
 
 # Register your models here.
+class StudentAdminModel(admin.ModelAdmin):
+    # models = Student
+    list_display = (
+        "email",
+        "username",
+        "gender",
+        "password",
+        "is_active"
+    )
+    search_fields = (
+        "email",
+        "username",
+    )
+    readonly_fields = (
+        "id",
+        "password",
+    )
+
+
+admin.site.register(Student, StudentAdminModel)
+# admin.site.register(Group, GroupAdmin)
+admin.site.register(Permission)
+admin.site.register(LogEntry)
+admin.site.register(ContentType)
+admin.site.register(Session)
