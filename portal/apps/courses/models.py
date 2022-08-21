1fdbcad3f3efd93949884c1e921b8e8d7bd99428
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-08-21 16:42:38 +0300

    chore: add course models

--- portal/apps/courses/models.py
+++ portal/apps/courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
